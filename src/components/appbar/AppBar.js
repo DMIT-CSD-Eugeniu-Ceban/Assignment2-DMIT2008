d518a9d5411494e3b01f9d0290231365abffa97b
Eugeniu Ceban <83615676+DMIT-CSD-Eugeniu-Ceban@users.noreply.github.com>
2022-02-25 02:03:15 -0700

    .

--- src/components/appbar/AppBar.js
+++ src/components/appbar/AppBar.js
@@ -3,27 +3,25 @@ import logo from './../../static/logo.png'
 
 import { IoEllipsisVertical, IoMailOutline, IoNotificationsOutline } from 'react-icons/io5'
 
-import { } from './styles'
+import { AppBarLogoStyle, AppBarLogo, AppBarButton, AppBarIcons, AppBarIcon } from './styles'
 
 function AppBar(props) {
     return (
-        <span>
-            <div>
-                <div>
-                    <span><img src={logo} alt="company logo" /></span>
-                    <h1>EC</h1>
-                </div>
-                <div>
-                    <ul>
-                        <span><button>sign out</button></span>
-                        <span><IoMailOutline size='1.5rem' /></span>
-                        <span><IoNotificationsOutline size='1.5rem' /></span>
-                        <span><img src='https://avatars.dicebear.com/api/initials/eugeniu%20ceban.svg?b=%23ef7534&r=50&scale=104' width='40' alt='Avatar' /></span>
-                        <span><IoEllipsisVertical size='1.5rem' /></span>
-                    </ul>
-                </div>
-            </div>
-        </span>
+        <AppBarLogoStyle bgcolor="#ECEFF1" color="#424242">
+            <AppBarLogo>
+                <span><img src={logo} width='60' style={{ margin: '1rem 0 1rem 1rem'}} alt="company logo" /></span>
+                <h1 style={{ fontSize: "2rem", fontWeight: 'bold', padding:'0 4rem 0 1rem'} }>EC</h1>
+            </AppBarLogo>
+            <AppBarIcons>
+                <AppBarIcon>
+                    <span><AppBarButton>sign out</AppBarButton></span>
+                    <span><IoMailOutline size='1.5rem' /></span>
+                    <span><IoNotificationsOutline size='1.5rem' /></span>
+                    <span><img src='https://avatars.dicebear.com/api/initials/eugeniu%20ceban.svg?b=%23ef7534&r=50&scale=104' width='35' alt='Avatar' /></span>
+                    <span><IoEllipsisVertical size='1.2rem' /></span>
+                </AppBarIcon>
+            </AppBarIcons>
+        </AppBarLogoStyle>
     );
 }
 
